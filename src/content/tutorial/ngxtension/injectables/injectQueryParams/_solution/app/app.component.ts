76fe77d0f5cbcbe6439e5a88d51e50e7ce4dc470
Michael Small <chickenhat22@gmail.com>
2024-08-01 21:50:36 -0500

    fix: query param routing

--- src/content/tutorial/ngxtension/injectables/injectQueryParams/_solution/app/app.component.ts
+++ src/content/tutorial/ngxtension/injectables/injectQueryParams/_solution/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+    <ul>
+      <li><a [routerLink]="['/queryParamsAccount', 1]" [queryParams]="{account: 1}">Queryparams Account 1</a></li>
+      <li><a [routerLink]="['/queryParamsAccount', 2]" [queryParams]="{account: 2}">Queryparams Account 2</a></li>
+      <li><a [routerLink]="['/queryParamsAccount', 3]" [queryParams]="{account: 3}">Queryparams Account 3</a></li>
+    </ul>
+
+    <router-outlet />
+  `,
 })
 export class AppComponent {
-  title = 'angular-template';
+    constructor(private router: Router) {
+        this.router.navigateByUrl('')
+    }
 }
