74155af6dea97d33d1b5135d415aa8a22397e8c8
Michael Small <chickenhat22@gmail.com>
2024-08-01 21:50:23 -0500

    feat: configure firebase + fix routing for query params

--- src/content/tutorial/ngxtension/injectables/injectQueryParams/_solution/app/app.component.ts
+++ src/content/tutorial/ngxtension/injectables/injectQueryParams/_solution/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { CodeStyledComponent } from './code-styled.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CodeStyledComponent, RouterModule],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
